ddb8c865e4ace4ed2e312cf16dd7d988a10cb8ac
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-06-26 09:24:56 +0100

    BI-13154 configure jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    roots: ["<rootDir>"],
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+    collectCoverageFrom: ["./src/**/*.ts"],
+    coveragePathIgnorePatterns: ["/src/bin/"],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: true,
+    testMatch: ["**/test/**/*.unit.[jt]s"],
+    globals: {
+        "ts-jest": {
+            diagnostics: false,
+        },
+    },
+    globalSetup: "./test/global.setup.ts",
+};
