6222b05c002e2e28cabf9e1782eb53fbf421c549
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-07-11 15:22:28 +0100

    streamlining files and updating config files

--- src/router.dispatch.ts
+++ src/router.dispatch.ts
@@ -1,9 +1,8 @@
 // Do Router dispatch here, i.e. map incoming routes to appropriate router
 import { Application, Request, Response } from "express";
-import indexRouter from "./routers/indexRouter";
 
 const routerDispatch = (app: Application) => {
-    app.use("/", indexRouter);
+    app.use("/", );
     app.use("*", (req: Request, res: Response) => {
         res.status(404).render("partials/error_400");
     });
