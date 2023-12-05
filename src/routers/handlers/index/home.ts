bc45b7fff29fa05e5fb7fdd8c6a0f19d8f120be9
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-12-05 15:27:06 +0000

    AOAF-8 refactor

--- src/routers/handlers/index/home.ts
+++ src/routers/handlers/index/home.ts
@@ -11,12 +11,10 @@ export class HomeHandler extends GenericHandler {
         this.viewData.sampleKey = "sample value for home page screen";
     }
 
-    execute (_req: Request, res: Response): Promise<Object> {
+    execute (_req: Request, res: Response): void {
         const submitLink: string = `${servicePathPrefix}${submitUrl}/`;
         const routeViews = "router_views/index";
         logger.info(`GET request for to serve home page`);
         res.render(`${routeViews}/home`, { ...this.viewData, submitLink });
-        // ...process request here and return data for the view
-        return Promise.resolve(this.viewData);
     }
 }
