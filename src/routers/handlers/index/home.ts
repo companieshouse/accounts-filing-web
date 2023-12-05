b03446ab75135004381b80822d4e41cbb5d21807
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-12-05 14:59:41 +0000

    AOAF-8 refactor

--- src/routers/handlers/index/home.ts
+++ src/routers/handlers/index/home.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
 import { GenericHandler } from "./../generic";
 import { logger } from "../../../utils/logger";
+import { servicePathPrefix, submitUrl } from "../../../utils/constants/urls";
 
 export class HomeHandler extends GenericHandler {
+
     constructor () {
         super();
         this.viewData.title = "Home handler for index router";
         this.viewData.sampleKey = "sample value for home page screen";
     }
 
-    execute (_req: Request, _response: Response): Promise<Object> {
+    execute (_req: Request, res: Response): Promise<Object> {
+        const submitLink: string = `${servicePathPrefix}${submitUrl}/`;
+        const routeViews = "router_views/index";
         logger.info(`GET request for to serve home page`);
+        res.render(`${routeViews}/home`, { ...this.viewData, submitLink });
         // ...process request here and return data for the view
         return Promise.resolve(this.viewData);
     }
--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,15 +1,11 @@
 import { Request, Response, Router, NextFunction } from "express";
 import { HomeHandler } from "./handlers/index/home";
-import { servicePathPrefix, submitUrl } from "../utils/constants/urls";
 
 const router: Router = Router();
-const routeViews = "router_views/index";
 
 router.get("/", async (req: Request, res: Response, _next: NextFunction) => {
     const handler = new HomeHandler();
-    const viewData = await handler.execute(req, res);
-    const submitLink: string = `${servicePathPrefix}${submitUrl}/`;
-    res.render(`${routeViews}/home`, { ...viewData, submitLink });
+    await handler.execute(req, res);
 });
 
 export default router;
