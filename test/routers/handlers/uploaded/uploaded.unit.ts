927f84edb0e4aec2db5c3fe79b14cad0f684342f
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-12-06 15:52:21 +0000

    Add unit tests

--- test/routers/handlers/uploaded/uploaded.unit.ts
+++ test/routers/handlers/uploaded/uploaded.unit.ts
@@ -1,18 +1,18 @@
 import { Request } from "express";
 import { UploadedHandler } from "../../../../src/routers/handlers/uploaded/uploaded";
 import { AccountValidatorResponse } from "private-api-sdk-node/dist/services/account-validator/types";
-import { accountValidationServiceMock } from "../../../mocks/account.validator.service.mock";
+import { accountsFilingServiceMock } from "../../../mocks/accounts.filing.service.mock";
 
 const validUUIDv4 = "bffebd2c-3848-43d2-a37a-78a93983ff52";
 
-describe('UploadedHandler', () => {
+describe("UploadedHandler", () => {
     let handler: UploadedHandler;
     let mockReq: Partial<Request>;
 
     beforeEach(() => {
         jest.clearAllMocks();
 
-        handler = new UploadedHandler(accountValidationServiceMock);
+        handler = new UploadedHandler(accountsFilingServiceMock);
         mockReq = {
             params: { fileId: validUUIDv4 },
         };
@@ -24,36 +24,53 @@ describe('UploadedHandler', () => {
         }
     }
 
-    describe('execute method', () => {
-        it('should call getValidationStatus with valid fileId', async () => {
+    describe("execute method", () => {
+        it("should call getValidationStatus with valid fileId", async () => {
             setRequestFileID(validUUIDv4);
-            accountValidationServiceMock.getValidationStatus.mockResolvedValue({ resource: {} as AccountValidatorResponse, httpStatusCode: 200 });
+            accountsFilingServiceMock.getValidationStatus.mockResolvedValue({
+                resource: {} as AccountValidatorResponse,
+                httpStatusCode: 200,
+            });
 
             await handler.execute(mockReq as Request, {} as any);
 
-            expect(accountValidationServiceMock.getValidationStatus).toHaveBeenCalledWith(validUUIDv4);
+            const expectedCallValue = {
+                accountsFilingId: "Placeholder accountsFilingId",
+                fileId: "bffebd2c-3848-43d2-a37a-78a93983ff52",
+                transactionId: "Placeholder transactionId",
+            };
+            expect(
+                accountsFilingServiceMock.getValidationStatus
+            ).toHaveBeenCalledWith(expectedCallValue);
         });
 
-        it('should return result with valid fileId', async () => {
+        it("should return result with valid fileId", async () => {
             setRequestFileID(validUUIDv4);
-            const mockResult = { resource: {} as AccountValidatorResponse, httpStatusCode: 200 };
-            accountValidationServiceMock.getValidationStatus.mockResolvedValue(mockResult);
+            const mockResult = {
+                resource: {} as AccountValidatorResponse,
+                httpStatusCode: 200,
+            };
+            accountsFilingServiceMock.getValidationStatus.mockResolvedValue(
+                mockResult
+            );
 
             const result = await handler.execute(mockReq as Request, {} as any);
 
             expect(result.result).toEqual(mockResult.resource);
         });
 
-        it('should not call getValidationStatus with invalid fileId', async () => {
-            setRequestFileID('invalid-uuid');
+        it("should not call getValidationStatus with invalid fileId", async () => {
+            setRequestFileID("invalid-uuid");
 
             await handler.execute(mockReq as Request, {} as any);
 
-            expect(accountValidationServiceMock.getValidationStatus).not.toHaveBeenCalled();
+            expect(
+                accountsFilingServiceMock.getValidationStatus
+            ).not.toHaveBeenCalled();
         });
 
-        it('should not return result with invalid fileId', async () => {
-            setRequestFileID('invalid-uuid');
+        it("should not return result with invalid fileId", async () => {
+            setRequestFileID("invalid-uuid");
 
             const result = await handler.execute(mockReq as Request, {} as any);
 
