2310f841ae33733c02c6943d371f9866c8508002
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-12-20 09:21:18 +0000

    Added a backUrl to the validator redirect

--- src/routers/submit.router.ts
+++ src/routers/submit.router.ts
@@ -5,8 +5,8 @@ const router: Router = Router();
 
 router.get('/', (req: Request, res: Response, _next: NextFunction) => {
     const submitHandler = new SubmitHandler();
-    const callbackUrl = submitHandler.execute(req, res);
-    res.redirect(callbackUrl);
+    const validatorRedriectUrl = submitHandler.execute(req, res);
+    res.redirect(validatorRedriectUrl);
 });
 
 export default router;
