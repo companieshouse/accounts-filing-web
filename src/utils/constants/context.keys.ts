755d174372e3f94ba56d394bf8416b4d59b40b14
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2024-03-12 18:46:35 +0000

    Fixed typo

--- src/utils/constants/context.keys.ts
+++ src/utils/constants/context.keys.ts
@@ -1,5 +1,5 @@
 export const ContextKeys = {
     ACCOUNTS_FILING_ID: "accountsFilingId",
     TRANSACTION_ID: "transactionId",
-    VALIDATION_STATUS: "vlidation_status"
+    VALIDATION_STATUS: "validation_status"
 };
