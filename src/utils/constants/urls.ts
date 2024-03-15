f9ab4b6b5ef1eced94da8ef58aa45e27bcc6dbf9
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-03-15 14:24:55 +0000

    urls

--- src/utils/constants/urls.ts
+++ src/utils/constants/urls.ts
@@ -6,12 +6,16 @@ export enum URL_QUERY_PARAM {
     PARAM_SUBMISSION_ID = "submissionId",
 }
 
-export const COMPANY_AUTH_PROTECTED_BASE = `/company/:${URL_QUERY_PARAM.PARAM_COMPANY_NUMBER}`;
-
 export const CONFIRM_COMPANY = '/confirm-company';
 export const CONFIRM_COMPANY_PATH = servicePathPrefix + CONFIRM_COMPANY;
 export const healthcheckUrl = '/healthcheck';
-export const uploadUrl = '/upload';
 export const fileIdPlaceholder = '{fileId}';
 export const uploadedUrl = '/uploaded';
 export const COMPANY_SEARCH = '/company-search';
+export const SIGN_OUT = '/signout';
+
+export const COMPANY_AUTH_PROTECTED_BASE = `/company/:${URL_QUERY_PARAM.PARAM_COMPANY_NUMBER}`;
+
+export const COMPANY_AUTH_UPLOAD = `${COMPANY_AUTH_PROTECTED_BASE}${uploadedUrl}`;
+
+export const COMPANY_LOOKUP = `/company-lookup/search?forward=${servicePathPrefix}${CONFIRM_COMPANY}?companyNumber=%7BcompanyNumber%7D`;
