bd2fb088a938a6ecfa4b3a2ec5363ed408002b6e
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-03-15 14:13:40 +0000

    new router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,5 +3,6 @@ import HealthCheckRouter from './healthcheck.router';
 import FileUpladedRouter from './file.uploaded.router';
 import UploadRouter from './upload.router';
 import CompanySearchRouter from './company.search.router';
+import CompanyConfirmRouter from './company.confirm.router';
 
-export { HealthCheckRouter, HomeRouter, FileUpladedRouter, UploadRouter, CompanySearchRouter };
+export { HealthCheckRouter, HomeRouter, FileUpladedRouter, UploadRouter, CompanySearchRouter, CompanyConfirmRouter };
