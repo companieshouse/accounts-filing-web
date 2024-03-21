37ef878ed438d6a8b524606dd3f50ae68cf6763b
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-03-21 10:03:02 +0000

    extra check

--- src/middleware/company.authentication.middleware.ts
+++ src/middleware/company.authentication.middleware.ts
@@ -2,10 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { env } from "../config";
 import { CompanyAuthenticationHelper } from "../utils/middleware/helper/company.authentication.helper";
+import { ValidateCompanyNumberFormat } from "../utils/validate/validate.company.number";
+import { logger } from "../utils/logger";
 
 export const companyAuthenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     const companyNumber = CompanyAuthenticationHelper.getCompanyNumberFromRequest(req);
+
+    if (!ValidateCompanyNumberFormat.isValid(companyNumber)){
+        logger.errorRequest(
+            req,
+            `Invalid company number format entered ${companyNumber}`
+        );
+        res.status(500).render("partials/error_500");
+    }
 
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: env.CHS_URL,
