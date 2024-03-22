9e0eb7a06a2fbdf73a9740826e22b5f6ec8538d8
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-03-22 08:53:01 +0000

    check for invalid company format

--- src/middleware/company.authentication.middleware.ts
+++ src/middleware/company.authentication.middleware.ts
@@ -2,10 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { env } from "../config";
 import { CompanyAuthenticationHelper } from "../utils/middleware/helper/company.authentication.helper";
+import { ValidateCompanyNumberFormat } from "../utils/validate/validate.company.number";
+import { logger } from "../utils/logger";
 
 export const companyAuthenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     const companyNumber = CompanyAuthenticationHelper.getCompanyNumberFromRequest(req);
+
+    if (!ValidateCompanyNumberFormat.isValid(companyNumber)){
+        logger.error(`An invalid company number format entered. Total length of input: ${companyNumber.length}. invalid format companyNumber = [${companyNumber}]`);
+        throw new Error("Company number is invalid");
+    }
 
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: env.CHS_URL,
