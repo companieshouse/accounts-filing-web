51b3a11af5d8d65c2e4dd0f8fe8a5d8c0b8f97e9
okoye-companieshouse <121234308+okoye-companieshouse@users.noreply.github.com>
2024-04-26 14:40:16 +0100

    AOAF-340 remove import (#73)
    * AOAF-340 remove import
    
    * AOAF-340 remove import

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { logger } from "./utils/logger";
 import cookieParser from "cookie-parser";
 import routerDispatch from "./router.dispatch";
 import { env } from './config';
-import { PrefixedUrls, SIGN_OUT } from "./utils/constants/urls";
+import { SIGN_OUT } from "./utils/constants/urls";
 import { formatToUKString, formatType } from "./utils/format/format";
 
 const app = express();
