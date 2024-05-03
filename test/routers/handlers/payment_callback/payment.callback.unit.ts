ce3095630a73579b421acc28b556411ab6c3b511
ch-skathirvel <skathirvel@companieshouse.gov.uk>
2024-05-03 17:52:58 +0100

    Added unit test case

--- test/routers/handlers/payment_callback/payment.callback.unit.ts
+++ test/routers/handlers/payment_callback/payment.callback.unit.ts
@@ -1,16 +1,40 @@
 import app from "../../../../src/app";
 import request from "supertest";
 import { PrefixedUrls } from "../../../../src/utils/constants/urls";
+import { mockAuthenticationMiddleware } from "../../../mocks/all.middleware.mock";
 
-describe("Payment call back test", () => {
-    it("Should render the check you answers page on payment failure get request", async () => {
-        const resp = await request(app).get(`${PrefixedUrls.PAYMENT_CALLBACK}?ref=Accounts_Filing_6634b9a82f93933ecb6a4745&state=123456&status=failed`);
-        expect(resp.status).toBe(302);
-        expect(resp.text).toContain(PrefixedUrls.CHECK_YOUR_ANSWERS);
-    });
-    it("Should render the comfirmation page on successful payment get request", async () => {
+
+describe("Payment callback tests", () => {
+
+    it("Should render the comfirmation page for successful payment", async () => {
         const resp = await request(app).get(`${PrefixedUrls.PAYMENT_CALLBACK}?ref=Accounts_Filing_6634b9a82f93933ecb6a4745&state=123456&status=paid`);
         expect(resp.status).toBe(302);
         expect(resp.text).toContain(PrefixedUrls.CONFIRMATION);
+        expect(resp.text).not.toContain(PrefixedUrls.CHECK_YOUR_ANSWERS);
+        expect(mockAuthenticationMiddleware).toHaveBeenCalled();
     });
+
+    it("Should render the check you answers page for failed payment", async () => {
+        const resp = await request(app).get(`${PrefixedUrls.PAYMENT_CALLBACK}?ref=Accounts_Filing_6634b9a82f93933ecb6a4745&state=123456&status=failed`);
+        expect(resp.status).toBe(302);
+        expect(resp.text).toContain(PrefixedUrls.CHECK_YOUR_ANSWERS);
+        expect(resp.text).not.toContain(PrefixedUrls.CONFIRMATION);
+        expect(mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
+    it("Should render the check you answers page for cancelled payment", async () => {
+        const resp = await request(app).get(`${PrefixedUrls.PAYMENT_CALLBACK}?ref=Accounts_Filing_6634b9a82f93933ecb6a4745&state=123456&status=cancelled`);
+        expect(resp.status).toBe(302);
+        expect(resp.text).toContain(PrefixedUrls.CHECK_YOUR_ANSWERS);
+        expect(resp.text).not.toContain(PrefixedUrls.CONFIRMATION);
+        expect(mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
+    it("should error if state doesn't match session state", async () => {
+        const resp = await request(app).get(`${PrefixedUrls.PAYMENT_CALLBACK}?ref=Accounts_Filing_6634b9a82f93933ecb6a4745&state=123123&status=paid`);
+        expect(resp.status).toBe(500);
+        expect(resp.text).toContain("Internal server error");
+        expect(mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
 });
