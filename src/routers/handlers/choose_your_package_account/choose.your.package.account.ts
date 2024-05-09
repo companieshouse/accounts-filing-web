f52551b0957fe04d86ccd13e302474e1bcfff318
okoye-companieshouse <121234308+okoye-companieshouse@users.noreply.github.com>
2024-05-09 09:43:55 +0100

    AOAF-221 package chooser page (#79)
    * AOAF-221 package chooser page
    
    * AOAF-221 package chooser page
    
    * AOAF-221 package chooser page
    
    * AOAF-221 package chooser page
    
    * AOAF-221 package chooser page
    
    * Fixing changed URL
    
    * resolving comments
    
    * resolving comments
    
    * resolving comments
    
    * renaming confusing vars

---
+++ src/routers/handlers/choose_your_package_account/choose.your.package.account.ts
@@ -0,0 +1,61 @@
+import { PrefixedUrls } from "../../../utils/constants/urls";
+import { getCompanyNumber, setPackageType } from "../../../utils/session";
+import { BaseViewData, GenericHandler } from "../generic";
+import { Request, Response } from "express";
+import { PackageAccountsType, getPackageItems, PackageTypeDetails } from "../../../utils/constants/PackageTypeDetails";
+import { logger } from "../../../utils/logger";
+import { PackageType } from "@companieshouse/api-sdk-node/dist/services/accounts-filing/types";
+
+interface ChooseYourPackageAccountsViewData extends BaseViewData {
+    packageAccountsItems: Array<PackageAccountsType>
+    packageAccounts?: PackageType
+}
+
+export class ChooseYourPackageAccountsHandler extends GenericHandler {
+    private packageAccountsItems = getPackageItems();
+    constructor() {
+        super({
+            title: "What package accounts are you submitting?",
+            backURL: null
+        });
+    }
+
+    async executePost (
+        req: Request,
+        _response: Response
+    ): Promise<ChooseYourPackageAccountsViewData> {
+        logger.info(`post request made for ${this.constructor.name}`);
+        super.populateViewData(req);
+        const companyNumber = getCompanyNumber(req.session);
+        const packageTypeChoice = req.body;
+
+        if ("value" in packageTypeChoice && packageTypeChoice["value"] in PackageTypeDetails){
+            setPackageType(req.session, packageTypeChoice);
+        } else {
+            this.baseViewData.errors.packageAccountsError = new Error("Package Accounts type must be set");
+        }
+
+        this.baseViewData.backURL = `${PrefixedUrls.CONFIRM_COMPANY}?companyNumber=${companyNumber}`;
+
+        return {
+            ...this.baseViewData,
+            packageAccountsItems: this.packageAccountsItems
+        };
+    }
+
+    async executeGet(
+        req: Request,
+        _response: Response
+    ): Promise<ChooseYourPackageAccountsViewData>{
+        logger.info(`get request made for ${this.constructor.name}`);
+        super.populateViewData(req);
+        const companyNumber = getCompanyNumber(req.session);
+
+        this.baseViewData.backURL = `${PrefixedUrls.CONFIRM_COMPANY}?companyNumber=${companyNumber}`;
+
+        return {
+            ...this.baseViewData,
+            packageAccountsItems: this.packageAccountsItems
+        };
+    }
+}
