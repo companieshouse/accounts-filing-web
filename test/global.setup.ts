d7a56c5180ca9e1e7e9e95effbf569609d440005
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2024-05-16 11:48:33 +0100

    Fix tests

--- test/global.setup.ts
+++ test/global.setup.ts
@@ -9,6 +9,7 @@ export default () => {
     process.env.COOKIE_DOMAIN = "cookie_domain";
     process.env.COOKIE_NAME = "cookie_name";
     process.env.COOKIE_SECRET = "123456789012345678901234";
+    process.env.FEEDBACK_LINK = "http://chs.local/feedback";
     process.env.INTERNAL_API_URL = "http://api.chs.local";
     process.env.NUNJUCKS_LOADER_WATCH = "false";
     process.env.SUBMIT_VALIDATION_URL = "http://chs.locl/xbrl_validate/submit";
