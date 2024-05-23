ffee7438d7a7eef5b442871089e849450ef0528f
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2024-05-23 09:26:21 +0100

    Docker updates

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM 416670754337.dkr.ecr.eu-west-2.amazonaws.com/ci-node-runtime-18
-
-RUN dnf install -y tar
+FROM node:18-bookworm
 
 WORKDIR /opt
-COPY dist ./package.json ./package-lock.json docker_start.sh ./
-COPY node_modules ./node_modules/
+
+COPY . . 
 
 CMD ["./docker_start.sh"]
 
--- docker_start.sh
+++ docker_start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Start script for accounts-filing-web
-PORT=3000
-export NODE_PORT=${PORT}
-exec node /opt/server.js -- ${PORT}
+
+echo "Arch: $(uname -m)"
+
+if [ ! -d "node_modules" ]; then
+    echo "node_modules directory does not exist. Attempting to install dependencies..."
+    if ! npm install; then
+        echo "npm install failed or timed out. Please run 'npm install' in the accounts-filing-web directory manually before starting the service."
+        exit 1
+    fi
+fi
+
+echo "Starting the accounts-filing-web service..."
+npm run dev
