ea297e99aeec39f4903cc31ca66afe0c775282b0
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-07-22 15:31:12 +0100

    Add single test and update welsh start-page

--- test/routers/home.unit.ts
+++ test/routers/home.unit.ts
@@ -10,4 +10,12 @@ describe("home page tests", () => {
         expect(resp.status).toBe(200);
         expect(resp.text).toContain(`File package accounts with Companies House`);
     });
+
+    it("should render the home page in welsh with a query of ?lang=cy", async () => {
+        const url = `${servicePathPrefix}`;
+        const resp = await request(app).get(url + "?lang=cy");
+
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain(`Bydd y gwasanaeth hwn yn dod i ben os na fyddwch yn ei ddefnyddio am 60 munud. Ni fydd eich atebion yn cael eu cadw.`);
+    });
 });
