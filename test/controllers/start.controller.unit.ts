8f433022ca4d8b3f505a8f252209fbadb0e25052
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-07-26 16:07:01 +0100

    AOAF-414 added language banner switcher

--- test/controllers/start.controller.unit.ts
+++ test/controllers/start.controller.unit.ts
@@ -1,6 +1,7 @@
 import { mockAuthenticationMiddleware } from "../mocks/all.middleware.mock";
 import request from "supertest";
 import app from "../../src/app";
+import { Urls, servicePathPrefix } from "../../src/utils/constants/urls";
 
 describe("start controller tests", () => {
 
@@ -24,9 +25,30 @@ describe("start controller tests", () => {
 
     it("should return start page with home ref to home link", async () => {
         const req = await request(app)
-            .get("/accounts-filing/");
+            .get(servicePathPrefix);
 
         expect(req.text).toContain(`<a href="/accounts-filing" class="govuk-header__link govuk-header__link--service-name">File package accounts with Companies House</a>`);
+    });
+
+    it("should translate `Support link` to Welsh on every page", async () => {
+
+        const urlList = Object.entries(Urls);
+        urlList.forEach(
+            async url => {
+                let req: {
+                    text: string
+                };
+                if (url[0] === "HOME"){
+                    req = await request(app)
+                        .get(servicePathPrefix + "?lang=cy");
+                } else {
+                    req = await request(app)
+                        .get(url[1] + "?lang=cy");
+                }
+
+                expect(req.text).toContain(`<a href="/accounts-filing" class="govuk-header__link govuk-header__link--service-name">File package accounts with Companies House</a>`);
+            }
+        );
     });
 
 });
