e07823856d363338a379d9332d907306bee2875a
ch-skathirvel <skathirvel@companieshouse.gov.uk>
2024-07-31 10:46:38 +0100

    Unit test cases

--- test/routers/home.unit.ts
+++ test/routers/home.unit.ts
@@ -1,6 +1,6 @@
 import app from "../../src/app";
 import request from "supertest";
-import { servicePathPrefix } from "../../src/utils/constants/urls";
+import { PrefixedUrls, servicePathPrefix } from "../../src/utils/constants/urls";
 
 describe("home page tests", () => {
     it("should render the home page", async () => {
@@ -9,13 +9,23 @@ describe("home page tests", () => {
 
         expect(resp.status).toBe(200);
         expect(resp.text).toContain(`File package accounts with Companies House`);
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
+    });
+
+    it("should render the home page", async () => {
+        const url = `${servicePathPrefix}`;
+        const resp = await request(app).get(url + "?lang=en");
+
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain(`File package accounts with Companies House`);
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
     });
 
     it("should render the home page in welsh with a query of ?lang=cy", async () => {
         const url = `${servicePathPrefix}`;
         const resp = await request(app).get(url + "?lang=cy");
-
         expect(resp.status).toBe(200);
-        expect(resp.text).toContain(`Bydd y gwasanaeth hwn yn dod i ben os na fyddwch yn ei ddefnyddio am 60 munud. Ni fydd eich atebion yn cael eu cadw.`);
+        expect(resp.text).toContain(`Ffeilio cyfrifon pecyn  gyda`);
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=cy");
     });
 });
