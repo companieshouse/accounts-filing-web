2ead3232ece774a60e8d895001409ecda30b1244
ch-skathirvel <136324774+ch-skathirvel@users.noreply.github.com>
2024-07-31 13:19:48 +0100

    JIRA: AOAF-415 (#109)
    JIRA: AOAF-415
    Description : Added welsh translation for before you file screen, also included the lang variable in URL when navigating from the home screen.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,8 @@ import { servicePathPrefix } from "./constants/urls";
 
 type Predicate<T> = (x: T) => boolean;
 
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
 export function skipIf(predicate: Predicate<Request>, handler: Handler): Handler {
     return (req, res, next) => {
         if (!predicate(req)) {
--- test/routers/before.you.file.package.accounts.unit.ts
+++ test/routers/before.you.file.package.accounts.unit.ts
@@ -2,23 +2,22 @@ import app from "../../src/app";
 import request from "supertest";
 import { PrefixedUrls } from "../../src/utils/constants/urls";
 
-
 describe("Before you file package accounts test", () => {
-
     it("Should render the page on get request", async () => {
         const resp = await request(app).get(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS);
         expect(resp.status).toBe(200);
         expect(resp.text).toContain("Before you file package accounts");
     });
-
+    it("Should render the page on get request", async () => {
+        const resp = await request(app).get(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain("Before you file package accounts");
+    });
 });
 
 describe("Welsh translation", () => {
     it("should translate `Support link` to Welsh for beforeYouFilePackageAccounts page", async () => {
-
-        const req = await request(app)
-            .get(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=cy");
-
-        expect(req.text).toContain("Dolenni cymorth");
+        const resp = await request(app).get(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=cy");
+        expect(resp.text).toContain("Cyn i chi ffeiio cyfrifon pecyn");
     });
 });
