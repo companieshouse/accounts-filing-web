cb28aca20d4dda972800e9e9c986108f940a23d9
okoye-companieshouse <121234308+okoye-companieshouse@users.noreply.github.com>
2024-11-08 14:30:17 +0000

    Aoaf 544 disabled package information (#130)
    * AOAF-544 managing disabled package flags
    
    * disabled package information on home screen
    
    * AOAF-544: remove unused import
    
    * AOAF-544: negative test
    
    * AOAF-545 disable payment info if both cic and overseas disabled
    
    * AOAF-544: fix logic problem in nunjucks

--- src/routers/handlers/index/home.ts
+++ src/routers/handlers/index/home.ts
@@ -4,6 +4,7 @@ import { logger } from "../../../utils/logger";
 import { fees } from "../../../utils/constants/fees";
 import { getLocalesField, addLangToUrl, selectLang } from "../../../utils/localise";
 import { PrefixedUrls } from "../../../utils/constants/urls";
+import { env } from "../../../config";
 
 export class HomeHandler extends GenericHandler {
 
@@ -18,10 +19,19 @@ export class HomeHandler extends GenericHandler {
 
     execute (_req: Request, _res: Response): ViewModel<HomeViewData> {
         const routeViews = "router_views/index";
-        logger.info(`GET request for to serve home page`);
+        logger.info(`GET request to serve home page`);
 
+        const disablePackageInfo = {
+            cic_disabled: env.CIC_DISABLE_RADIO,
+            overseas_disabled: env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO,
+            audit_exempt_disabled: env.DISABLE_AUDIT_EXEMPT_SUBSIDIARY_ACCOUNTS_RADIO,
+            dormant_disabled: env.DISABLE_DORMANT_EXEMPT_SUBSIDIARY_ACCOUNTS_RADIO,
+            limited_disabled: env.DISABLE_LIMITED_PARTNERSHIP_ACCOUNTS_RADIO,
+            group_400_disabled: env.DISABLE_GROUP_SECTION_400_UK_PARENT_ACCOUNTS_RADIO,
+            group_401_disabled: env.DISABLE_GROUP_SECTION_401_NON_UK_PARENT_ACCOUNTS_RADIO
+        };
 
-        return { templatePath: `${routeViews}/home`, viewData: { ...this.baseViewData, fees } };
+        return { templatePath: `${routeViews}/home`, viewData: { ...this.baseViewData, fees, ...disablePackageInfo } };
     }
 }
 
--- test/routers/home.unit.ts
+++ test/routers/home.unit.ts
@@ -1,8 +1,35 @@
-import app from "../../src/app";
 import request from "supertest";
 import { PrefixedUrls, servicePathPrefix } from "../../src/utils/constants/urls";
+import { loadApp } from "./load.app";
 
 describe("home page tests", () => {
+    let app;
+
+    const resetEnvVariables = () => {
+        delete process.env.DISABLE_DORMANT_EXEMPT_SUBSIDIARY_ACCOUNTS_RADIO;
+        delete process.env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO;
+        delete process.env.DISABLE_GROUP_SECTION_400_UK_PARENT_ACCOUNTS_RADIO;
+        delete process.env.DISABLE_GROUP_SECTION_401_NON_UK_PARENT_ACCOUNTS_RADIO;
+        delete process.env.DISABLE_LIMITED_PARTNERSHIP_ACCOUNTS_RADIO;
+        delete process.env.CIC_DISABLE_RADIO;
+
+        process.env.DISABLE_DORMANT_EXEMPT_SUBSIDIARY_ACCOUNTS_RADIO = "false";
+        process.env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO = "false";
+        process.env.DISABLE_GROUP_SECTION_400_UK_PARENT_ACCOUNTS_RADIO = "false";
+        process.env.DISABLE_GROUP_SECTION_401_NON_UK_PARENT_ACCOUNTS_RADIO = "false";
+        process.env.DISABLE_LIMITED_PARTNERSHIP_ACCOUNTS_RADIO = "false";
+        process.env.CIC_DISABLE_RADIO = "false";
+    };
+
+    beforeEach(() => {
+        resetEnvVariables();
+        app = loadApp();
+    });
+
+    afterEach(() => {
+        resetEnvVariables();
+    });
+
     it("should render the home page", async () => {
         const url = `${servicePathPrefix}`;
         const resp = await request(app).get(url);
@@ -27,5 +54,64 @@ describe("home page tests", () => {
         expect(resp.status).toBe(200);
         expect(resp.text).toContain(`Ffeilio cyfrifon pecyn  gyda`);
         expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=cy");
+    });
+
+    it("should render the home page with all accounts text displayed", async () => {
+        const url = `${servicePathPrefix}`;
+        const resp = await request(app).get(url);
+
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain('UKSEF accounts for listed companies');
+        expect(resp.text).toContain('Welsh accounts with an English translation');
+        expect(resp.text).toContain('dormant exempt subsidiary accounts');
+        expect(resp.text).toContain('group package accounts - section 400, parent incorporated under UK law');
+        expect(resp.text).toContain('Community Interest Companies (CIC) - there is a £15 fee to file');
+        expect(resp.text).toContain('overseas companies - there is a £33  fee to file');
+        expect(resp.text).toContain('limited partnership accounts');
+        expect(resp.text).toContain('group package accounts - section 401, parent incorporated under non-UK law');
+        expect(resp.text).toContain('to pay the fee using a credit or debit card, if relevant');
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
+    });
+
+    it("should render the home page with only uksef and welsh accounts text displayed", async () => {
+
+        process.env.DISABLE_DORMANT_EXEMPT_SUBSIDIARY_ACCOUNTS_RADIO = "true";
+        process.env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO = "true";
+        process.env.DISABLE_GROUP_SECTION_400_UK_PARENT_ACCOUNTS_RADIO = "true";
+        process.env.DISABLE_GROUP_SECTION_401_NON_UK_PARENT_ACCOUNTS_RADIO = "true";
+        process.env.DISABLE_LIMITED_PARTNERSHIP_ACCOUNTS_RADIO = "true";
+        process.env.CIC_DISABLE_RADIO = "true";
+
+        const url = `${servicePathPrefix}`;
+        app = loadApp();
+
+        const resp = await request(app).get(url);
+
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain('UKSEF accounts for listed companies');
+        expect(resp.text).toContain('Welsh accounts with an English translation');
+        expect(resp.text).not.toContain('dormant exempt subsidiary accounts');
+        expect(resp.text).not.toContain('group package accounts - section 400, parent incorporated under UK law');
+        expect(resp.text).not.toContain('Community Interest Companies (CIC) - there is a £15 fee to file');
+        expect(resp.text).not.toContain('overseas companies - there is a £33  fee to file');
+        expect(resp.text).not.toContain('limited partnership accounts');
+        expect(resp.text).not.toContain('group package accounts - section 401, parent incorporated under non-UK law');
+        expect(resp.text).not.toContain('to pay the fee using a credit or debit card, if relevant');
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
+    });
+
+    it("should display payment text when either cic or overseas enabled", async () => {
+
+        process.env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO = ["true", "false"][Math.floor(Math.random() * 2)];
+        process.env.CIC_DISABLE_RADIO = (process.env.DISABLE_OVERSEAS_COMPANY_ACCOUNTS_RADIO !== "true").toString();
+
+        const url = `${servicePathPrefix}`;
+        app = loadApp();
+
+        const resp = await request(app).get(url);
+
+        expect(resp.status).toBe(200);
+        expect(resp.text).toContain(PrefixedUrls.BEFORE_YOU_FILE_PACKAGE_ACCOUNTS + "?lang=en");
+        expect(resp.text).toContain('to pay the fee using a credit or debit card, if relevant');
     });
 });
