82ed136d6caff9a0fe6265c3e52128fc00973962
lwalker-ch <lwalker@companieshouse.gov.uk>
2024-11-18 17:41:57 +0000

    Updated Terraform modules

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,54 +19,57 @@ terraform {
 
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.275"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.294"
 
-  name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
   kms_key_id  = data.aws_kms_key.kms_key.id
+  name_prefix = "${local.service_name}-${var.environment}"
   secrets     = nonsensitive(local.service_secrets)
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.275"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.294"
 
   # Environmental configuration
-  environment             = var.environment
-  aws_region              = var.aws_region
   aws_profile             = var.aws_profile
-  vpc_id                  = data.aws_vpc.vpc.id
+  aws_region              = var.aws_region
   ecs_cluster_id          = data.aws_ecs_cluster.ecs_cluster.id
+  environment             = var.environment
   task_execution_role_arn = data.aws_iam_role.ecs_cluster_iam_role.arn
+  vpc_id                  = data.aws_vpc.vpc.id
 
   # Load balancer configuration
   lb_listener_arn           = data.aws_lb_listener.filing_maintain_lb_listener.arn
-  lb_listener_rule_priority = local.lb_listener_rule_priority
   lb_listener_paths         = local.lb_listener_paths
+  lb_listener_rule_priority = local.lb_listener_rule_priority
 
   # Healthcheck configuration
+  health_check_grace_period_seconds = 300
+  healthcheck_healthy_threshold     = "2"
   healthcheck_path                  = local.healthcheck_path
   healthcheck_matcher               = local.healthcheck_matcher
   use_task_container_healthcheck    = true
-  health_check_grace_period_seconds = 300
-  healthcheck_healthy_threshold     = "2"
 
   # Cloudwatch
   cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
 
   # Docker container details
+  container_port    = local.container_port
+  container_version = var.accounts_filing_web_version
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
-  container_version = var.accounts_filing_web_version
-  container_port    = local.container_port
+  read_only_root_filesystem = true
+
 
   # Service configuration
-  service_name = local.service_name
   name_prefix  = local.name_prefix
-
-  # Service Healthcheck configuration
+  service_name = local.service_name
 
   # Service performance and scaling configs
   desired_task_count                  = var.desired_task_count
+  fargate_subnets                     = local.application_subnet_ids
+  max_task_count                      = var.max_task_count
+  min_task_count                      = var.min_task_count
   required_cpus                       = var.required_cpus
   required_memory                     = var.required_memory
   service_autoscale_enabled           = var.service_autoscale_enabled
@@ -75,12 +78,10 @@ module "ecs-service" {
   service_scaleup_schedule            = var.service_scaleup_schedule
   use_capacity_provider               = var.use_capacity_provider
   use_fargate                         = var.use_fargate
-  fargate_subnets                     = local.application_subnet_ids
 
   # Service environment variable and secret configs
   task_environment = local.task_environment
   task_secrets     = local.task_secrets
-  app_environment_filename  = local.app_environment_filename
   use_set_environment_files = local.use_set_environment_files
 
   depends_on = [module.secrets]
