e5c841e142ddcef5db4a99a738d237f790d40386
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-12-05 10:20:23 +0000

    AOAF-567: add unit tests for extract company number middleware

--- src/middleware/extract.company.number.middleware.ts
+++ src/middleware/extract.company.number.middleware.ts
@@ -1,15 +1,18 @@
 import { Request, Response, NextFunction } from "express";
-import { checkCompanyNumberFormatIsValidate } from "utils/format/company.number.format";
+import { checkCompanyNumberFormatIsValidate } from "../../src/utils/format/company.number.format";
 
 export const extractCompanyNumberMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const companyNumber = req.params.companyNumber;
 
     if (companyNumber) {
+        try {
+            checkCompanyNumberFormatIsValidate(companyNumber);
 
-        checkCompanyNumberFormatIsValidate(companyNumber);
-
-        if (req.session) {
-            req.session.setExtraData("companyNumber", companyNumber);
+            if (req.session) {
+                req.session.setExtraData("companyNumber", companyNumber);
+            }
+        } catch (error) {
+            return next(error);
         }
     }
 
