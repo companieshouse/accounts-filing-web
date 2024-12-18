f4a03c136a07121665229767bddd210224e6ba34
hbosworth <hbosworth@companieshouse.gov.uk>
2024-12-18 09:54:04 +0000

    Refactor home router

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -3,20 +3,15 @@ import { HomeHandler } from "./handlers/index/home";
 
 const router = Router();
 
-router.get("/",  (req: Request, res: Response) => {
+function renderHomePage(req: Request, res: Response) {
     const handler = new HomeHandler(req);
     const params = handler.execute(req, res);
     if (params.templatePath && params.viewData) {
         res.render(params.templatePath, params.viewData);
     }
-});
+}
 
-router.get("/company/:companyNumber",  (req: Request, res: Response) => {
-    const handler = new HomeHandler(req);
-    const params = handler.execute(req, res);
-    if (params.templatePath && params.viewData) {
-        res.render(params.templatePath, params.viewData);
-    }
-});
+router.get("/",  renderHomePage);
+router.get("/company/:companyNumber",  renderHomePage);
 
 export default router;
