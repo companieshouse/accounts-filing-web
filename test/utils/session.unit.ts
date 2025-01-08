a19f325ce1b75222582f4144075960abaf8c93ed
hbosworth <hbosworth@companieshouse.gov.uk>
2025-01-08 15:25:35 +0000

    Add unit tests for clearSession

--- test/utils/session.unit.ts
+++ test/utils/session.unit.ts
@@ -1,6 +1,7 @@
 import { Session } from '@companieshouse/node-session-handler';
 import { getSessionRequest, testAccessToken } from '../mocks/session.mock';
-import { getSignInInfo, getAccessToken } from "../../src/utils/session";
+import { getSignInInfo, getAccessToken, clearSession } from "../../src/utils/session";
+import { ContextKeys } from '../../src/utils/constants/context.keys';
 
 describe('SessionUtils test suite', () => {
     const testSessionRequest: Session = getSessionRequest();
@@ -13,5 +14,32 @@ describe('SessionUtils test suite', () => {
     it('Test function getAccessToken()', () => {
         const signInInfo = getAccessToken(testSessionRequest);
         expect(signInInfo).toEqual(testAccessToken.access_token);
+    });
+
+    describe('clearSession', () => {
+        let mockSession: Session;
+
+        beforeEach(() => {
+            mockSession = {
+                deleteExtraData: jest.fn(),
+                getExtraData: jest.fn()
+            } as unknown as Session;
+        });
+
+        it('should clear all specified session data', () => {
+            clearSession(mockSession);
+
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.TRANSACTION_ID);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.ACCOUNTS_FILING_ID);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.PACKAGE_TYPE);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.COMPANY_NAME);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.COMPANY_NUMBER);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.LANGUAGE);
+            expect(mockSession.deleteExtraData).toHaveBeenCalledWith(ContextKeys.IS_CHS_JOURNEY);
+        });
+
+        it('should throw an error if session is undefined', () => {
+            expect(() => clearSession(undefined)).toThrow('Unable to clear session as session is undefined');
+        });
     });
 });
