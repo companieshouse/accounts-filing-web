8eda259fa8300ac7d1a1ac40871405b628bbe3a4
jbox-CH <jbox@companieshouse.gov.uk>
2026-09-23 15:30:40 +0100

    corrected old test

--- test/routers/cannot.file.package.accounts.for.company.type.unit.ts
+++ test/routers/cannot.file.package.accounts.for.company.type.unit.ts
@@ -8,11 +8,10 @@ describe("cannot file full accounts for company type page", () => {
     });
 
     it("should render stop page with required content and guidance link", async () => {
-        const response = await getRequestWithCookie(PrefixedUrls.CANNOT_FILE_FULL_ACCOUNTS_FOR_COMPANY_TYPE);
+        const response = await getRequestWithCookie(PrefixedUrls.CANNOT_FILE_PACKAGE_ACCOUNTS_FOR_COMPANY_TYPE);
 
         expect(response.statusCode).toBe(200);
-        expect(response.text).toContain("You cannot file full accounts for this company type");
-        expect(response.text).toContain("You cannot file accounts for a UK Establishment of an overseas company. Only the overseas company can file accounts.");
+        expect(response.text).toContain("cannot file package accounts");
         expect(response.text).toContain("https://www.gov.uk/file-accounts-in-the-uk-as-an-overseas-company");
         expect(response.text).toContain(PrefixedUrls.COMPANY_SEARCH);
     });
